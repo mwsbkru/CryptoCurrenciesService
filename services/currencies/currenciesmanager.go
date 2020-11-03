0036b63e5e6f7bcfaf713b7e8865955e14929b5d
m.sychev <m.w.s@bk.ru>
2020-11-03 15:35:24 +0300

    finished manager

--- services/currencies/currenciesmanager.go
+++ services/currencies/currenciesmanager.go
@@ -1,17 +1,56 @@
 package currencies
 
 import (
-	"models"
+	"../../models"
 )
 
 type CurrenciesManager struct {
 	remoteDataProvider IRemoteDataProvider
 	cacheDataProvider ICacheDataProvider
+	cacheDataValidator ICacheDataValidator
+	errorsReporter IErrorsReporter
+	asyncRunner IAsyncTaskRunner
 }
 
-func GetCurrencyRate(code string) models.CurrencyRate {
-	return nil
+func (manager CurrenciesManager) GetCurrencyRate(code string) models.CurrencyRate, err {
+	currencyRate, err := manager.cacheDataProvider.GetCachedCurrency(code)
+	if err != nil || !manager.cacheDataValidator.isValid(currencyRate) {
+		currencyRate, err = manager.prepareNewValue(code)
+
+		if err != nil {
+			manager.errorsReporter.ReportError(err)
+			return currencyRate, errors.New("Can`t fetch any data about currency rate!")
+		}
+
+		return currencyRate, nil 
+	}
+	
+	manager.updateCachedCurrencyAsync(code)
+	return currencyRate, nil
 }
+
+func (manager CurrenciesManager) prepareNewValue(code string) models.CurrencyRate, err {
+	loadedCurrencyRate, err := manager.remoteDataProvider.GetCurrencyRate(code)
+
+	if err != nil {
+		return loadedCurrencyRate, err
+	}
+
+	err := manager.cacheDataProvider.StoreCurrency(code, loadedCurrencyRate)
+
+	if err != nil {
+		manager.errorsReporter.ReportError(err)
+	}
+
+	return loadedCurrencyRate, nil
+}
+
+func (manager CurrenciesManager) updateCachedCurrencyAsync(code string) {
+	manager.asyncRunner.Run(func() {
+		manager.prepareNewValue(code)
+	})
+}
+
 func GetCachedCurrencyRates() []models.CurrencyRate {
 	return nil
 }
--- services/currencies/types.go
+++ services/currencies/types.go
@@ -1,4 +1,27 @@
 package currencies
 
-type IRemoteDataProvider struct {}
-type ICacheDataProvider struct {}
+import "models"
+
+type IRemoteDataProvider interface {
+	GetCurrencyRate(code string) models.CurrencyRate
+}
+
+type ICacheDataProvider interface {
+	GetCachedCurrency(code string) models.CurrencyRate
+	StoreCurrency(code string, currencyRate models.CurrencyRate) error
+	GetStoredCurrencies() []models.CurrencyRate
+}
+
+type ICacheDataValidator interface {
+	IsValid(currency models.CurrencyRate) bool
+}
+
+type IErrorsReporter interface {
+	ReportError(err error)
+}
+
+type IAsyncTaskRunner interface {
+	RunTask(task AsyncTastFunc)
+}
+
+type AsyncTastFunc func()
